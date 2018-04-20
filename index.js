23a5d0300441c5c894c13f6c3e4c5689458664cc
Justin Tooley <jstooley823@gmail.com>
2018-04-20 17:34:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function getRepositories(){
   req.open("GET", 'https://api.github.com/users/'+ username +'/repos')
   req.send()
 }
-function showRepositories(event, data) {
+function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Display Commits</a></li>').join('')}</ul>`
