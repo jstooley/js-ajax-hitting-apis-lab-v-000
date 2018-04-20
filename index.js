98ef90864256a2fa257aa5348b103be1d25fa5b9
Justin Tooley <jstooley823@gmail.com>
2018-04-20 17:24:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function displayCommits() {
 function getCommits(el) {
   const name = el.dataset.repo
   const req = new XMLHttpRequest()
-  req.addEventListener("load", dispalyCommits)
+  req.addEventListener("load", displayCommits)
   req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
   req.send()
 }
