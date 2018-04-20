2bb5a2419a253bf8e27fc256638bf17c1c936930
Justin Tooley <jstooley823@gmail.com>
2018-04-20 17:36:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function getCommits(el) {
   const username = el.dataset.username
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayCommits)
-  req.open("GET", `https://api.github.com/repos/${username}/${name}/commits`)
+  req.open("GET", `https://api.github.com/repos/${ username }/${ name }/commits`)
   req.send()
 }
 function getBranches(el) {
