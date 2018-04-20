abc7eaa243a4898b4c4a7e94332e2e7a7a984dcf
Justin Tooley <jstooley823@gmail.com>
2018-04-20 17:31:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function displayCommits() {
   document.getElementById("details").innerHTML = commitsList
 }
 function getCommits(el) {
-  const name = el.dataset.repo
+  const name = el.dataset.repository
   const username = el.dataset.username
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayCommits)
