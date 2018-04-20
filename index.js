c549ace543a81744f77d47a787452efb83d387fb
Justin Tooley <jstooley823@gmail.com>
2018-04-20 17:23:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ function showRepositories(event, data) {
 }
 function displayCommits() {
   const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-  
+  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
 }
 function getCommits(el) {
